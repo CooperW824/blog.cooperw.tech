27f216e6b4a8f59fa7df686d75d31501c3ee8b41
Cooper Werner <cooperwerner824@gmail.com>
2023-12-20 21:35:33 -0500

    Setting Up DaisyUI and Color Palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,25 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          "primary": "#07337d",
+          "secondary": "#7a99d6",
+          "accent": "#14ebff",
+          "neutral": "#1f232d",
+          "base-100": "#dbdee6",
+        },
+        dark: {
+          "primary": "#82aef8",
+          "secondary": "#284683",
+          "accent": "#00D7EB",
+          "neutral": "#1f242e",
+          "base-100": "#191d25",
+        },
+      },
+    ],
+  },
 }
