14d991c6b4de2058ad5a67b2a939d1463f1f4233
Cooper Werner <cooperwerner824@gmail.com>
2023-12-22 14:49:39 -0500

    Add Share Link to Headings

--- components/heading.vue
+++ components/heading.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="text-primary font-semibold">
+	<div class="text-primary font-semibold flex w-fit h-fit items-center" :id="component.content.toLocaleLowerCase().replace(/ /g, '-')">
 		<div v-if="component.size === 2" class="w-fit flex flex-col items-center justify-center">
 			<h2 class="text-3xl mb-0.5">
 				{{ component.content }}
@@ -18,6 +18,14 @@
 		<h6 class="text-md" v-else-if="component.size === 6">
 			{{ component.content }}
 		</h6>
+		<button
+			@click="copyLink"
+			class="ml-2 w-8 h-8 text-primary hover:text-accent duration-300 ease-in-out active:w-10 active:h-10 transition-all"
+			aria-label="Copy Heading Share Link"
+			title="Copy Heading Share Link"
+		>
+			<font-awesome-icon :icon="['fas', 'link']" class="w-full h-full" />
+		</button>
 	</div>
 </template>
 
@@ -27,4 +35,13 @@ import type { heading } from '~/types/blog';
 
 const props = defineProps<{ content: heading }>();
 const component = ref(props.content);
+
+const copyLink = () => {
+	let link = window.location.href;
+	// remove any existing hash
+	link = link.split('#')[0];
+	const URI = encodeURI(component.value.content.toLocaleLowerCase().replace(/ /g, '-'));
+	link += `#${URI}`;
+	navigator.clipboard.writeText(link);
+};
 </script>
